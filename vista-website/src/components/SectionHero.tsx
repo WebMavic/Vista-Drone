218e4a1b112cb9a79e4c2be81de96ed906b3f739
Kumar Srijan <49157668+kumarsrijan@users.noreply.github.com>
2024-05-23 12:28:45 +0530

    videos added
    as

--- vista-website/src/components/SectionHero.tsx
+++ vista-website/src/components/SectionHero.tsx
@@ -3,17 +3,18 @@ import { ChevronRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import HeroImage from "./HeroImage";
 
+
 function SectionHero() {
   return (
-    <section className="heropattern  w-full bg-opacity-[10%] bg-cover bg-no-repeat px-5 sm:h-screen sm:bg-contain sm:bg-center sm:px-32">
+    <section className="w-full  px-5 sm:h-screen sm:px-32 heropattern bg-opacity-[10%] sm:bg-center sm:bg-contain bg-cover bg-no-repeat">
       <div className="flex h-full w-full flex-wrap items-center gap-4 py-24 sm:flex-nowrap sm:justify-between  ">
         <div className="space-y-5">
-          <h1 className="sm:max:w-3/4 text-3xl font-bold leading-tight  text-heading sm:text-6xl">
+          <h1 className="text-3xl font-bold leading-tight text-heading  sm:max:w-3/4 sm:text-6xl">
             Empowering the future with drone technology
           </h1>
           <p className="text text-lg text-subheading sm:w-1/2">
-            Staying competitive in today's market, we must continuously innovate
-            and improve our products and services.
+            lorem ipsum dolor sit amet consectetur adipiscing elit Curabitur
+            imperdiet.
           </p>
 
           <Link
@@ -24,8 +25,9 @@ function SectionHero() {
             <span className="uppercase tracking-wider">Discover</span>
           </Link>
         </div>
+       
+        <HeroImage className="sm:w-1/2 w-full "/>
 
-        <HeroImage className="w-full sm:w-1/2 " />
       </div>
     </section>
   );
--- vista-website/src/utils/images.js
+++ vista-website/src/utils/images.js
@@ -1,29 +1,27 @@
 export const clientsLogos = {
-  client1: require("../assets/images/clients-logo/img8.jpg"),
-  client2: require("../assets/images/clients-logo/img10.jpg"),
-  client3: require("../assets/images/clients-logo/img11.jpg"),
-  client4: require("../assets/images/clients-logo/img13.jpg"),
-  client5: require("../assets/images/clients-logo/img15.jpg"),
-  client6: require("../assets/images/clients-logo/img16.jpg"),
-  client7: require("../assets/images/clients-logo/img18.jpg"),
-  client8: require("../assets/images/clients-logo/img19.jpg"),
-  client9: require("../assets/images/clients-logo/img20.jpg"),
-  client10: require("../assets/images/clients-logo/img21.jpg"),
-  client11: require("../assets/images/clients-logo/img22.jpg"),
-  client12: require("../assets/images/clients-logo/img23.jpg"),
-  client13: require("../assets/images/clients-logo/img24.jpg"),
-  client14: require("../assets/images/clients-logo/img25.jpg"),
-  client15: require("../assets/images/clients-logo/img26.jpg"),
+    client1: require('../assets/images/clients-logo/img8.jpg'),
+    client2: require('../assets/images/clients-logo/img10.jpg'),
+    client3: require('../assets/images/clients-logo/img11.jpg'),
+    client4: require('../assets/images/clients-logo/img13.jpg'),
+    client5: require('../assets/images/clients-logo/img15.jpg'),
+    client6: require('../assets/images/clients-logo/img16.jpg'),
+    client7: require('../assets/images/clients-logo/img18.jpg'),
+    client8: require('../assets/images/clients-logo/img19.jpg'),
+    client9: require('../assets/images/clients-logo/img20.jpg'),
+    client10: require('../assets/images/clients-logo/img21.jpg'),
+    client11: require('../assets/images/clients-logo/img22.jpg'),
+    client12: require('../assets/images/clients-logo/img23.jpg'),
+    client13: require('../assets/images/clients-logo/img24.jpg'),
+    client14: require('../assets/images/clients-logo/img25.jpg'),
+    client15: require('../assets/images/clients-logo/img26.jpg')
 };
+
 
 //if you want to add image make sure it is 500x500px
 export const droneImage = [
-  require("../assets/images/drone1.png"),
-  require("../assets/images/drone2.png"),
-];
+    require('../assets/images/drone1.png'),
+    require('../assets/images/drone2.png'),
+    
+    
 
-export const expertiseImage = [
-  require("../assets/images/why-choose-us-1.jpeg"),
-  require("../assets/images/why-choose-us-2.jpeg"),
-  require("../assets/images/why-choose-us-3.1.jpeg"),
-];
+]
